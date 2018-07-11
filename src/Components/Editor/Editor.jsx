57c1753220d4e87c0fc93d275dcf27946380d1cf
Luca <ionix@protonmail.com>
2018-07-11 14:21:19 +0200

    implement basic assembler

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -6,9 +6,33 @@ import { Grid } from 'semantic-ui-react';
 import 'brace/mode/assembly_x86';
 import 'brace/theme/monokai';
 
+const ks = window.ks;
+const asm = new ks.Keystone(ks.ARCH_X86, ks.MODE_64);
+asm.option(ks.OPT_SYNTAX, ks.OPT_SYNTAX_INTEL);
+
 export default class Editor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { bytes: [] };
+  }
+
   onChange = (value) => {
-    console.log('change', value);
+    let instructions = value.split('\n');
+    let bytes = [];
+    instructions.forEach(instruction => {
+      try {
+        let result = asm.asm(instruction);
+        if(!result.failed) {
+          let inst = '';
+          result.forEach(byte => {
+            inst += '0x' + ('0' + (Number(byte).toString(16))).slice(-2).toUpperCase() + ' ';
+          });
+          bytes.push(inst);
+        }
+      } catch(ex) { /* don't destroy ace */ }
+    });
+    window.ace.edit('bytes').setValue(bytes.join('\n').split(' ,').join('\n'));
   }
 
   render() {
@@ -23,7 +47,7 @@ export default class Editor extends Component {
             theme='monokai'
             onChange={this.onChange}
             name='editor'
-            editorProps={{$blockScrolling: true}}
+            editorProps={{$blockScrolling: Infinity}}
             width='100%'
             fontSize={24}
           />
@@ -33,7 +57,7 @@ export default class Editor extends Component {
             mode='assembly_x86'
             theme='monokai'
             name='bytes'
-            editorProps={{$blockScrolling: true}}
+            editorProps={{$blockScrolling: Infinity}}
             width='100%'
             fontSize={24}
             readOnly={true}
